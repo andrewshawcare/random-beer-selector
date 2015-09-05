4f1b5d372621af530e2973ccb47fd2ca1983ee7c
Andrew Shaw Care <andrewshawcare@gmail.com>
2015-09-05 18:32:45 -0500

    Move store, products, and inventories out of Random Beer Selector

--- app/index.spec.js
+++ app/index.spec.js
@@ -1,66 +1,101 @@
 define(["./index.js"], function (RandomBeerSelector) {
-    describe("Random Beer Selector", function () {
-      var randomBeerSelector;
+  describe("Random Beer Selector", function () {
+    var store = {id: 511};
+    var products = [
+      { id: 1, primary_category: "Beer", is_dead: false, is_discontinued: false, inventory_count: 1 },
+      { id: 2, primary_category: "Beer", is_dead: false, is_discontinued: false, inventory_count: 1 },
+      { id: 3, primary_category: "Beer", is_dead: false, is_discontinued: false, inventory_count: 1 },
+      { id: 4, primary_category: "Beer", is_dead: true, is_discontinued: false, inventory_count: 1 },
+      { id: 5, primary_category: "Beer", is_dead: false, is_discontinued: true, inventory_count: 1 },
+      { id: 6, primary_category: "Beer", is_dead: false, is_discontinued: false, inventory_count: 0 },
+      { id: 7, primary_category: "Wine", is_dead: false, is_discontinued: false, inventory_count: 1 },
+      { id: 8, primary_category: "Beer", is_dead: false, is_discontinued: false, inventory_count: 1 },
+      { id: 9, primary_category: "Beer", is_dead: false, is_discontinued: false, inventory_count: 1 }
+    ];
+    var inventories = [
+      { product_id: 1, store_id: 511, quantity: 1 },
+      { product_id: 2, store_id: 511, quantity: 1 },
+      { product_id: 3, store_id: 511, quantity: 0 },
+      { product_id: 4, store_id: 511, quantity: 1 },
+      { product_id: 5, store_id: 511, quantity: 1 },
+      { product_id: 6, store_id: 511, quantity: 1 },
+      { product_id: 7, store_id: 511, quantity: 1 },
+      { product_id: 9, store_id: 512, quantity: 1 }
+    ];
+    var randomBeerSelector;
 
-      beforeEach(function () {
-        randomBeerSelector = RandomBeerSelector();
+    beforeEach(function () {
+      randomBeerSelector = RandomBeerSelector({
+        store: store,
+        products: products,
+        inventories: inventories
       });
-
-        it("should be an object", function () {
-            expect(randomBeerSelector).toBeDefined();
-            expect(randomBeerSelector).not.toBeNull();
-            expect(typeof randomBeerSelector).toBe("object");
-        });
-
-        it("should select a beer", function () {
-          var selection;
-          while (randomBeerSelector.canSelect()) {
-            selection = randomBeerSelector.select();
-            expect(selection.product.primary_category).toBe("Beer");
-          }
-        });
-
-        it("should select an available product", function () {
-            var selection;
-            while (randomBeerSelector.canSelect()) {
-              selection = randomBeerSelector.select();
-              expect(selection.product.is_dead).toBe(false);
-              expect(selection.product.is_discontinued).toBe(false);
-              expect(selection.product.inventory_count).toBeGreaterThan(0);
-            }
-        });
-
-        it("should select a product from store 511", function () {
-          var selection;
-          while (randomBeerSelector.canSelect()) {
-            var selection = randomBeerSelector.select();
-            expect(selection.store.id).toBe(511);
-          }
-        });
-
-        it("should select a product with inventory at the selected store", function () {
-          var selection;
-          while (randomBeerSelector.canSelect()) {
-            selection = randomBeerSelector.select();
-            expect(selection.product.id).toEqual(selection.inventory.product_id);
-            expect(selection.store.id).toBe(selection.inventory.store_id);
-            expect(selection.inventory.quantity).toBeGreaterThan(0);
-          }
-        });
-
-        it("should not select the same product", function () {
-          var selectedProductIds = {};
-          var selection;
-          while (randomBeerSelector.canSelect()) {
-            selection = randomBeerSelector.select();
-            expect(selectedProductIds.hasOwnProperty(selection.product.id)).toBe(false);
-            selectedProductIds[selection.product.id] = true;
-          }
-        });
-
-        it("should return undefined if no more selections are available", function () {
-          while (randomBeerSelector.canSelect() && randomBeerSelector.select()) {}
-          expect(randomBeerSelector.select()).not.toBeDefined();
-        });
     });
+
+    it("should be a function", function () {
+      expect(RandomBeerSelector).toBeDefined();
+      expect(typeof RandomBeerSelector).toBe("function");
+    });
+
+    it("should work without arguments", function () {
+      expect(RandomBeerSelector).not.toThrow();
+    });
+
+    it("should produce an object", function () {
+      expect(randomBeerSelector).toBeDefined();
+      expect(randomBeerSelector).not.toBeNull();
+      expect(typeof randomBeerSelector).toBe("object");
+    });
+
+    it("should select a beer", function () {
+      var selection;
+      while (randomBeerSelector.canSelect()) {
+        selection = randomBeerSelector.select();
+        expect(selection.product.primary_category).toBe("Beer");
+      }
+    });
+
+    it("should select an available product", function () {
+      var selection;
+      while (randomBeerSelector.canSelect()) {
+        selection = randomBeerSelector.select();
+        expect(selection.product.is_dead).toBe(false);
+        expect(selection.product.is_discontinued).toBe(false);
+        expect(selection.product.inventory_count).toBeGreaterThan(0);
+      }
+    });
+
+    it("should select a product from store 511", function () {
+      var selection;
+      while (randomBeerSelector.canSelect()) {
+        var selection = randomBeerSelector.select();
+        expect(selection.store.id).toBe(511);
+      }
+    });
+
+    it("should select a product with inventory at the selected store", function () {
+      var selection;
+      while (randomBeerSelector.canSelect()) {
+        selection = randomBeerSelector.select();
+        expect(selection.product.id).toEqual(selection.inventory.product_id);
+        expect(selection.store.id).toBe(selection.inventory.store_id);
+        expect(selection.inventory.quantity).toBeGreaterThan(0);
+      }
+    });
+
+    it("should not select the same product", function () {
+      var selectedProductIds = {};
+      var selection;
+      while (randomBeerSelector.canSelect()) {
+        selection = randomBeerSelector.select();
+        expect(selectedProductIds.hasOwnProperty(selection.product.id)).toBe(false);
+        selectedProductIds[selection.product.id] = true;
+      }
+    });
+
+    it("should return undefined if no more selections are available", function () {
+      while (randomBeerSelector.canSelect() && randomBeerSelector.select()) {}
+      expect(randomBeerSelector.select()).not.toBeDefined();
+    });
+  });
 });
