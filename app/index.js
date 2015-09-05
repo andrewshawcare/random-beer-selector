6038294cfce838a86d4a0671255d1d5a3b8dccbd
Andrew Shaw Care <andrewshawcare@gmail.com>
2015-09-04 22:33:38 -0500

    Random Beer Selector should not select the same product twice

--- app/index.js
+++ app/index.js
@@ -1,17 +1,34 @@
 define([], function () {
+  return function () {
+    var products = [
+      {
+        id: 1,
+        primary_category: "Beer",
+        is_dead: false,
+        is_discontinued: false,
+        inventory_count: 1
+      },
+      {
+        id: 2,
+        primary_category: "Beer",
+        is_dead: false,
+        is_discontinued: false,
+        inventory_count: 1
+      }
+    ];
     return {
-        select: function () {
-            return {
-              store: {
-                id: 511
-              },
-              product: {
-                primary_category: "Beer",
-                is_dead: false,
-                is_discontinued: false,
-                inventory_count: 1
-              }
-            };
-        }
+      canSelect: function () {
+        return products.length > 0;
+      },
+      select: function () {
+        var randomProductIndex = Math.floor(Math.random() * products.length);
+        return {
+          store: {
+            id: 511
+          },
+          product: products.splice(randomProductIndex, 1)[0]
+        };
+      }
     };
+  };
 });
--- app/index.spec.js
+++ app/index.spec.js
@@ -1,24 +1,40 @@
 define(["./index.js"], function (RandomBeerSelector) {
     describe("Random Beer Selector", function () {
+      var randomBeerSelector;
+
+      beforeEach(function () {
+        randomBeerSelector = RandomBeerSelector();
+      });
+
         it("should be defined", function () {
-            expect(RandomBeerSelector).toBeDefined();
+            expect(randomBeerSelector).toBeDefined();
         });
 
         it("should select a beer", function () {
-            var selection = RandomBeerSelector.select();
+            var selection = randomBeerSelector.select();
             expect(selection.product.primary_category).toBe("Beer");
         });
 
         it("should select an available product", function () {
-            var selection = RandomBeerSelector.select();
+            var selection = randomBeerSelector.select();
             expect(selection.product.is_dead).toBe(false);
             expect(selection.product.is_discontinued).toBe(false);
             expect(selection.product.inventory_count).toBeGreaterThan(0);
         });
 
         it("should select a product from store 511", function () {
-          var selection = RandomBeerSelector.select();
+          var selection = randomBeerSelector.select();
           expect(selection.store.id).toBe(511);
+        });
+
+        it("should not select the same product", function () {
+          var selectedProductIds = {};
+          var selection;
+          while (randomBeerSelector.canSelect()) {
+            selection = randomBeerSelector.select();
+            expect(selectedProductIds.hasOwnProperty(selection.product.id)).toBe(false);
+            selectedProductIds[selection.product.id] = true;
+          }
         });
     });
 });
