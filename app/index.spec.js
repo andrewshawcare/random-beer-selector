62ecfbfd94f34425012f0e40ca830fb04358c682
Andrew Shaw Care <andrewshawcare@gmail.com>
2015-09-05 17:16:50 -0500

    Exhaust selection for each test

--- app/index.spec.js
+++ app/index.spec.js
@@ -13,27 +13,39 @@ define(["./index.js"], function (RandomBeerSelector) {
         });
 
         it("should select a beer", function () {
-            var selection = randomBeerSelector.select();
+          var selection;
+          while (randomBeerSelector.canSelect()) {
+            selection = randomBeerSelector.select();
             expect(selection.product.primary_category).toBe("Beer");
+          }
         });
 
         it("should select an available product", function () {
-            var selection = randomBeerSelector.select();
-            expect(selection.product.is_dead).toBe(false);
-            expect(selection.product.is_discontinued).toBe(false);
-            expect(selection.product.inventory_count).toBeGreaterThan(0);
+            var selection;
+            while (randomBeerSelector.canSelect()) {
+              selection = randomBeerSelector.select();
+              expect(selection.product.is_dead).toBe(false);
+              expect(selection.product.is_discontinued).toBe(false);
+              expect(selection.product.inventory_count).toBeGreaterThan(0);
+            }
         });
 
         it("should select a product from store 511", function () {
-          var selection = randomBeerSelector.select();
-          expect(selection.store.id).toBe(511);
+          var selection;
+          while (randomBeerSelector.canSelect()) {
+            var selection = randomBeerSelector.select();
+            expect(selection.store.id).toBe(511);
+          }
         });
 
         it("should select a product with inventory at the selected store", function () {
-          var selection = randomBeerSelector.select();
-          expect(selection.product.id).toEqual(selection.inventory.product_id);
-          expect(selection.store.id).toBe(selection.inventory.store_id);
-          expect(selection.inventory.quantity).toBeGreaterThan(0);
+          var selection;
+          while (randomBeerSelector.canSelect()) {
+            selection = randomBeerSelector.select();
+            expect(selection.product.id).toEqual(selection.inventory.product_id);
+            expect(selection.store.id).toBe(selection.inventory.store_id);
+            expect(selection.inventory.quantity).toBeGreaterThan(0);
+          }
         });
 
         it("should not select the same product", function () {
