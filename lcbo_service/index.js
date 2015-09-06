31d0bf1bbaabe0a28b0de1adc83956579ece2da3
Andrew Shaw Care <andrewshawcare@gmail.com>
2015-09-05 23:19:40 -0500

    Add service call for products

--- lcbo_service/index.js
+++ lcbo_service/index.js
@@ -1,8 +1,20 @@
-define(["lib/es6-promise"], function (Es6Promise) {
+define(["lib/es6-promise", "lib/jquery"], function (Es6Promise, $) {
   return {
-    getProducts: function () {
+    getProducts: function (args) {
+      var store_id = args.store_id;
       return new Promise(function (resolve, reject) {
-        resolve([]);
+        $.ajax({
+          url: "http://lcboapi.com/products",
+          dataType: "jsonp",
+          data: {
+            per_page: 100,
+            store_id: store_id
+          }
+        })
+          .then(function (response) {
+            resolve(response.result);
+          })
+          .fail(reject);
       });
     }
   };
